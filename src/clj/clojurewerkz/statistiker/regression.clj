cde64e74edc1aaab9ce7216df4b1694d24fc5739
Alex Petrov <alexp@coffeenco.de>
2014-03-09 20:01:48 +0100

    Add linear regression

--- src/clj/clojurewerkz/statistiker/regression.clj
+++ src/clj/clojurewerkz/statistiker/regression.clj
@@ -1,2 +1,16 @@
 (ns clojurewerkz.statistiker.regression
+  (:import [org.apache.commons.math3.stat.regression SimpleRegression])
   (:require [clojurewerkz.statistiker.fast-math :refer :all]))
+
+(defn linear-regression
+  "Performs a linear regression"
+  ([data]
+     (linear-regression first second))
+  ([data field1-extractor field2-extractor]
+     (let [regression (SimpleRegression. true)
+           matrix (into-array (map double-array (map vector
+                                                     (map #(get % field1-extractor) data)
+                                                     (map #(get % field2-extractor) data))))]
+       (.addData regression matrix)
+       {:intercept (.getIntercept regression)
+        :slope     (.getSlope regression)})))
