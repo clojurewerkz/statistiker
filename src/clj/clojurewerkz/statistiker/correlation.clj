39e9e313ca97a4d293fe1b64f4356febc4c37aa2
Alex Petrov <alexp@coffeenco.de>
2014-03-26 14:39:36 +0100

    Add pearsons correlation, detrending and best-repetition-patterns

---
+++ src/clj/clojurewerkz/statistiker/correlation.clj
@@ -0,0 +1,30 @@
+(ns clojurewerkz.statistiker.correlation
+  (:import [org.apache.commons.math3.stat.correlation PearsonsCorrelation])
+  (:require [clojure.math.combinatorics :as combo]
+            [clojurewerkz.statistiker.summary :refer :all]
+            [clojurewerkz.statistiker.descriptive :refer :all]))
+
+(defn pearsons-correlation
+  [v1 v2]
+  (let [instance (PearsonsCorrelation.)]
+    (.correlation instance (double-array v1) (double-array v2))))
+
+(defn best-repetition-patterns
+  [v correlation]
+  (let [preprocessed (for [i (range 2 (inc (/ (count v) 2)))]
+                       (let [partitions   (partition i v)
+                             combinations (combo/combinations partitions 2)]
+                         [i (mean (map (fn [[v1 v2]] (pearsons-correlation v1 v2)) combinations))]))]
+    (->> preprocessed
+         (remove #(.isNaN (last %)))
+         (remove #(> 0 (last %)))
+         (sort-by last >))))
+
+(defn detrend
+  [v cycle]
+  (->> v
+       (partition cycle)
+       (map (fn [s]
+              (let [m (apply min s)]
+                (map #(- % m) s))))
+       (mapcat identity)))
