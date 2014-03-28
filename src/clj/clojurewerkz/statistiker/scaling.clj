e85964465b8be43997757d4a12ec3b0fa74e9d96
Alex Petrov <alexp@coffeenco.de>
2014-03-28 12:10:58 +0100

    Add scaling

---
+++ src/clj/clojurewerkz/statistiker/scaling.clj
@@ -0,0 +1,18 @@
+(ns clojurewerkz.statistiker.scaling
+  (:require [clojurewerkz.statistiker.summary :as summary]))
+
+(defn rescale
+  "Linear transformation"
+  [x]
+  (let [xmin        (apply min x)
+        xmax        (apply max x)
+        scale-fn (fn [x] (double (/ (- x xmin) (- xmax xmin))))]
+    (mapv scale-fn x)))
+
+(defn z-transform
+  "Z-Transformation"
+  [x]
+  (let [xmean    (summary/mean x)
+        xsd      (summary/sd x)
+        scale-fn (fn [x] (double (/ (- x xmean) xsd)))]
+    (map scale-fn x)))
