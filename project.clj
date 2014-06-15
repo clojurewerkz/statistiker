e049aea16a7154b9b1246c391a006a20012956b7
Alex Petrov <alexp@coffeenco.de>
2014-06-15 23:39:25 +0200

    Update math3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.combinatorics "0.0.7"]
-                 [org.apache.commons/commons-math3 "3.2"]
+                 [org.apache.commons/commons-math3 "3.3"]
                  [net.mikera/core.matrix "0.20.0"]
                  [org.jblas/jblas "1.2.3"]]
   :source-paths       ["src/clj"]
