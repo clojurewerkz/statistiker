dea372d2b8c2e874f2e05c2610cd49082859e216
Alex Petrov <alexp@coffeenco.de>
2014-06-22 23:22:31 +0200

    Add more scaling optiosn

--- src/clj/clojurewerkz/statistiker/scaling.clj
+++ src/clj/clojurewerkz/statistiker/scaling.clj
@@ -1,17 +1,74 @@
 (ns clojurewerkz.statistiker.scaling
-  (:require [clojurewerkz.statistiker.statistics :refer [mean sd]]))
+  (:require [clojurewerkz.statistiker.statistics :refer [mean sd]]
+            [clojurewerkz.statistiker.fast-math :refer [sqr]]))
+
+(defn make-rescale-range-fn
+  [x xmin xmax]
+  (let [orig-min  (apply min x)
+        orig-max  (apply max x)
+        orig-diff (- orig-max orig-min)
+        diff      (- xmax xmin)]
+    (fn [x]
+      (double (+ (/ (* (- x orig-min)
+                       diff)
+                    orig-diff)
+                 xmin
+                 )))))
+
+(defn make-rescale-fn
+  [x]
+  (let [xmin (apply min x)
+        xmax (apply max x)
+        diff (- xmax xmin) ]
+    (fn [x] (double (/ (- x xmin) diff)))))
 
 (defn rescale
-  "Linear transformation"
   [x]
-  (let [xmin        (apply min x)
-        xmax        (apply max x)
-        scale-fn (fn [x] (double (/ (- x xmin) (- xmax xmin))))]
-    (mapv scale-fn x)))
+  (mapv (make-rescale-fn x) x))
+
+(defn rescale-range
+  [x xmin xmax]
+  (mapv (make-rescale-range-fn x xmin xmax) x))
+
+(defn make-standartise-fn
+  [x]
+  (let [xmean    (mean x)
+        xsd      (sd x)]
+    (fn [x] (double (/ (- x xmean) xsd)))))
 
 (defn standartise
   [x]
-  (let [xmean    (mean x)
-        xsd      (sd x)
-        scale-fn (fn [x] (double (/ (- x xmean) xsd)))]
-    (map scale-fn x)))
+  (mapv (make-standartise-fn x) x))
+
+(defn make-l1-normalize-fn
+  [x]
+  (let [sum (->> x
+                 (map #(Math/abs %))
+                 (reduce +))]
+    #(/ % sum)))
+
+(defn l1-normalize
+  "L1-normalize (divide each element by sum of absolute values)."
+  [x]
+  (mapv (make-l1-normalize-fn x) x))
+
+(defn make-l2-normalize-fn
+  [x]
+  (let [sum (->> x
+                 (map #(sqr %))
+                 (reduce +))]
+    #(/ % sum)))
+
+(defn l2-normalize
+  "L2-normalize (divide each element by sum of squares)"
+  [x]
+  (mapv (make-l2-normalize-fn x) x))
+
+(defn scale-feature
+  [maps key scale-fn-factory]
+  (let [extracted (map #(get % key) maps)
+        scale-f   (scale-fn-factory extracted)
+        scaled    (map scale-f extracted)
+        ]
+    (map (fn [v scaled]
+           (assoc v key scaled)) maps scaled)))
