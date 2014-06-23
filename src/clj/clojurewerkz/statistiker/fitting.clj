361d816b5c1518696d9e23097163872546560820
Alex Petrov <alexp@coffeenco.de>
2014-06-23 23:48:24 +0200

    Add gaussian fitting

--- src/clj/clojurewerkz/statistiker/fitting.clj
+++ src/clj/clojurewerkz/statistiker/fitting.clj
@@ -13,3 +13,19 @@
 (defn gaussian-function
   [norm mean sigma]
   (Gaussian. norm mean sigma))
+
+(defn fit
+  "Extract x and y from dataset, and compose an approximated, fitted dataset from interpolated points, taking
+   `steps` points."
+  [dss x y steps]
+  (let [prepared          (map (fn [i] [(get i x)
+                                       (get i y)]) dss)
+        min-x             (reduce min (map (fn [i] (get i x)) dss))
+        max-x             (reduce max (map (fn [i] (get i x)) dss))
+        step              (/ (- max-x min-x) steps)
+        [norm mean sigma] (gaussian-fitting prepared)
+        f                 (gaussian-function norm mean sigma)]
+    (mapv
+     (fn [x-val]
+       {x x-val y (.value f (double x-val))})
+     (take steps (iterate #(+ % step) min-x)))))
