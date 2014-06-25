f9e4966e214579f0d1e310095fa1fd3cfbeaaf23
Alex Petrov <alexp@coffeenco.de>
2014-06-26 00:30:13 +0200

    STart working on regressions

--- src/clj/clojurewerkz/statistiker/regression.clj
+++ src/clj/clojurewerkz/statistiker/regression.clj
@@ -11,5 +11,20 @@
                                                          (map #(get % field1-extractor) data)
                                                          (map #(get % field2-extractor) data))))]
        (.addData regression matrix)
-       {:intercept (.getIntercept regression)
-        :slope     (.getSlope regression)})))
+       regression
+       )))
+
+
+(defn predict
+  [regression]
+  (.predict regression))
+
+(defn to-map
+  []
+  {:intercept          (.getIntercept regression)
+   :slope              (.getSlope regression)
+   :sum-squared-errors (.getSumSquaredErrors regression)
+   :total-sum-squares  (.getTotalSumSquares regression)
+   :intercept-std-err  (.getInterceptStdErr regression)
+   :slope-std-err      (.getSlopeStdErr regression)
+   })
