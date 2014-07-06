b6e6e32d3aa760fa623e05caab4145948f5e0bdf
Alex Petrov <alexp@coffeenco.de>
2014-07-06 23:23:03 +0200

    Remove unused project

--- project.clj
+++ project.clj
@@ -7,8 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.combinatorics "0.0.7"]
                  [org.apache.commons/commons-math3 "3.3"]
-                 [net.mikera/core.matrix "0.20.0"]
-                 [org.jblas/jblas "1.2.3"]]
+                 [net.mikera/core.matrix "0.20.0"]]
   :source-paths       ["src/clj"]
   :java-source-paths  ["src/java"]
   :test-paths         ["test/clj"])
