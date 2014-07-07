5fefbb276a5e033e5e2dad2b7a2ce426c6f9bf3a
Alex Petrov <alexp@coffeenco.de>
2014-07-07 23:21:00 +0200

    Add missing deps

--- project.clj
+++ project.clj
@@ -4,10 +4,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.combinatorics "0.0.7"]
+  :dependencies [[org.clojure/clojure              "1.5.1"]
+                 [org.clojure/math.combinatorics   "0.0.7"]
                  [org.apache.commons/commons-math3 "3.3"]
-                 [net.mikera/core.matrix "0.20.0"]]
+                 [net.mikera/core.matrix           "0.20.0"]
+
+                 [prismatic/schema                 "0.2.2"]]
+  :core.typed {:check [clojurewerkz.statistiker.optimization]}
   :source-paths       ["src/clj"]
   :java-source-paths  ["src/java"]
   :test-paths         ["test/clj"])
