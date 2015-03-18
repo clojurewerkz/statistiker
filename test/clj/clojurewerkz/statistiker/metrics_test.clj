0ab6d28653f4692d051571668331f12d05e9c048
David Corney <dpacorney@gmail.com>
2015-03-18 16:24:48 +0000

    Extra tests

--- test/clj/clojurewerkz/statistiker/metrics_test.clj
+++ test/clj/clojurewerkz/statistiker/metrics_test.clj
@@ -1,21 +1,23 @@
 (ns clojurewerkz.statistiker.metrics-test
-	(:require [clojurewerkz.statistiker.metrics :refer :all]            
+	(:require [clojurewerkz.statistiker.metrics :refer :all]
             [clojure.test :refer :all]))
 
 
 (deftest wrong-input-ami
 	(is (thrown? AssertionError (adjusted_mutual_information [1 2 3] [2 3])))
 	(is (thrown? AssertionError (adjusted_mutual_information [] [2 3])))
-	(is (thrown? AssertionError (adjusted_mutual_information [:b :a :c] [2 :a]))))
+	(is (thrown? AssertionError (adjusted_mutual_information [:b :a :c] [2 :a])))
+  (is (thrown? AssertionError (adjusted_mutual_information [[:b :a] :c] [2 :a]))))
 
 (deftest wrong-input-mi
 	(is (thrown? AssertionError (mutual_information [1 2 3] [2 3])))
 	(is (thrown? AssertionError (mutual_information [] [2 3])))
 	(is (thrown? AssertionError (mutual_information [:b :a :c] [2 :a])))
-	(is (thrown? AssertionError (mutual_information [] []))))
+	(is (thrown? AssertionError (mutual_information [] [])))
+  (is (thrown? AssertionError (adjusted_mutual_information [[:b :a] :c] [2 :a]))))
 
 (deftest adjusted_mutual_information-values
-	(is (= (adjusted_mutual_information [1 2 3] [1 2 3]) 
+	(is (= (adjusted_mutual_information [1 2 3] [1 2 3])
 			(adjusted_mutual_information [3 2 1] [1 2 3])
 			(adjusted_mutual_information [1 2 3] [:a :b 2])
 			1.0))
@@ -29,7 +31,7 @@
 
 
 (deftest mutual_information-values
-	(is (= (mutual_information [1 2 3] [1 2 3]) 
+	(is (= (mutual_information [1 2 3] [1 2 3])
 			(mutual_information [3 2 1] [1 2 3])
 			(mutual_information [1 2 3] [:a :b 2])
 			1.0986122886681096))
