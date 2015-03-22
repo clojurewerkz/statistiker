624ba95064ee57e4ed6ad94649bad2364273d7ff
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-22 17:57:17 +0100

    Update math conbinatorics

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure              "1.5.1"]
-                 [org.clojure/math.combinatorics   "0.0.7"]
+  :dependencies [[org.clojure/clojure              "1.6.0"]
+                 [org.clojure/math.combinatorics   "0.1.1"]
                  [org.apache.commons/commons-math3 "3.4.1"]
                  [net.mikera/core.matrix           "0.33.2"]
-
                  [prismatic/schema                 "0.2.2"]]
   :core.typed {:check [clojurewerkz.statistiker.optimization]}
   :source-paths       ["src/clj"]
