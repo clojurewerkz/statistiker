3445e7b02b0051d485df9bf646025ba9fc9bab00
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-22 17:56:27 +0100

    Update core.matrics

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure              "1.5.1"]
                  [org.clojure/math.combinatorics   "0.0.7"]
                  [org.apache.commons/commons-math3 "3.4.1"]
-                 [net.mikera/core.matrix           "0.20.0"]
+                 [net.mikera/core.matrix           "0.33.2"]
 
                  [prismatic/schema                 "0.2.2"]]
   :core.typed {:check [clojurewerkz.statistiker.optimization]}
