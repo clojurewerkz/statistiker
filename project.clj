57c187a8da5aa5004c78c74a143ab31b9174e5be
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-22 17:55:19 +0100

    Update math3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure              "1.5.1"]
                  [org.clojure/math.combinatorics   "0.0.7"]
-                 [org.apache.commons/commons-math3 "3.3"]
+                 [org.apache.commons/commons-math3 "3.4.1"]
                  [net.mikera/core.matrix           "0.20.0"]
 
                  [prismatic/schema                 "0.2.2"]]
